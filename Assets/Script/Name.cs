3a60e4d30ece66ea825df296ad4af93b5b714ab2
李爽 <aeolian76@gmail.com>
2020-09-22 18:45:24 +0800

    name

---
+++ Assets/Script/Name.cs
@@ -0,0 +1,42 @@
+﻿public static class Name
+{
+    public enum SpriteName
+    {
+        Empty,
+        Wall,
+        Baba,
+        Flag,
+        Rock,
+        WallWord,
+        BabaWord,
+        FlagWord,
+        RockWord,
+        IsWord,
+        WinWord,
+        YouWord,
+        PushWord,
+        StopWord,
+    }
+    public enum ObjectWords
+    {
+        WallWord,
+        BabaWord,
+        FlagWord,
+        RockWord,
+    }
+
+    public enum IsWord 
+    {
+        IsWord 
+    }
+
+    public enum ProperWords
+    {
+        WinWord,
+        YouWord,
+        PushWord,
+        StopWord,
+    }
+
+}
+
