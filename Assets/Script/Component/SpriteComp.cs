a910a6d70eff7daae415d18c82d5d86acbba3507
李爽 <aeolian76@gmail.com>
2020-09-22 18:55:44 +0800

    文件修改

---
+++ Assets/Script/Component/SpriteComp.cs
@@ -0,0 +1,15 @@
+﻿using Entitas;
+
+public class SpriteComp : IComponent
+{
+    public Name.SpriteName name
+    {
+        get;
+        private set;
+    }
+
+    public void SetValue(Name.SpriteName n)
+    {
+        name = n;
+    }
+}
